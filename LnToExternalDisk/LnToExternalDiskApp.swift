d989b63aee8e71eb47b69122d7d82c3d70075a4a
ricardo <znyh1219@gmail.com>
2026-04-15 10:58:41 +0800

    Initial Commit

---
+++ LnToExternalDisk/LnToExternalDiskApp.swift
@@ -0,0 +1,17 @@
+//
+//  LnToExternalDiskApp.swift
+//  LnToExternalDisk
+//
+//  Created by Ricardo on 13/4/2026.
+//
+
+import SwiftUI
+
+@main
+struct LnToExternalDiskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
